ca7ca575e853ec1677ad271e898e6f5167c5220c
Murat Kilic <muratkilic1978@gmail.com>
2024-09-04 10:59:08 +0200

    function updateTotalPrice(product) was added to script.js and HTML was updated with quantity and totalPrice input-fields

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@
 function addToCart(product) {
     let quantity = document.getElementById(product).value;
     document.getElementById(product).value = parseInt(quantity) + 1;
+
+    updateTotalPrice(product); // opdater totalsum
+
 }
 
 // Function to remove one item from the cart for a specific product and update the input field
@@ -9,6 +12,8 @@ function removeFromCart(product) {
     let quantity = document.getElementById(product).value;
     if (quantity > 0) {
         document.getElementById(product).value = parseInt(quantity) - 1;
+
+        updateTotalPrice(product); // opdater totalsum
     }
 }
 
@@ -16,10 +21,31 @@ function removeFromCart(product) {
 function increaseQuantity(product, amount) {
     let quantity = document.getElementById(product).value;
     document.getElementById(product).value = parseInt(quantity) + amount;
+
+    updateTotalPrice(product); // opdater totalsum
 }
 
 // Function to reset the cart quantity to 0 for a specific product and update the input field
 function resetCart(product) {
     document.getElementById(product).value = 0;
+
+    updateTotalPrice(product); // opdater totalsum
 }
+
+function updateTotalPrice(product) 
+{
+    const quantityElement = document.getElementById(product);
+    const priceElement = document.getElementById(product + '-price');
+    const totalElement = document.getElementById(product+ '-total');
+
+    if ( quantityElement && priceElement && totalElement ) {
+        const quantity = parseInt(quantityElement.value);
+        const price = parseInt(priceElement.value);
+        const total = quantity * price;
+
+        totalElement.value = total;
+    
+    }
+}
+
 
